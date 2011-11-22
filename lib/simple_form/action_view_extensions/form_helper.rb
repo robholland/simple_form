d1a36a67297ed68565430cf06b5dc9f4dafb019a
Rob Holland <rob@the-it-refinery.co.uk>
2011-11-22 11:37:06 +0000

    Mix simple_form into ActionView::Helpers rather than Base to make it more available to third party code.

--- lib/simple_form/action_view_extensions/form_helper.rb
+++ lib/simple_form/action_view_extensions/form_helper.rb
@@ -64,4 +64,4 @@ module SimpleForm
   end
 end
 
-ActionView::Base.send :include, SimpleForm::ActionViewExtensions::FormHelper
+ActionView::Helpers.send :include, SimpleForm::ActionViewExtensions::FormHelper
